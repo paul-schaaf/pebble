f3439d50d23a9803682c1a61a36a2f48628f1e01
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-18 23:51:21 +0100

    prettier now works on save

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div>Hello</div>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
