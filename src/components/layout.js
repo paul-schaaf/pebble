61e150b58992b60ece4d8e876c919c66b7452da8
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 00:54:35 +0100

    deleted superfluous layout content and css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,18 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import Header from './header'
-import './layout.css'
+import Header from './header';
+import './layout.css';
 
 const Layout = ({ children }) => (
   <>
     <Header siteTitle="Pebble" />
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0px 1.0875rem 1.45rem`,
-        paddingTop: 0,
-      }}
-    >
-      {children}
-    </div>
+    <div>{children}</div>
   </>
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
