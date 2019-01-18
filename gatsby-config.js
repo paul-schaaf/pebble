b49cae807d4b6970e87b706f01e77e45bdb575de
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-18 20:23:16 +0100

    deleted template files and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Pebble`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@paulschaaf`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
