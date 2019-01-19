cc64898846c3bfbf57a13fab883ac69ecfa7eb16
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 13:22:59 +0100

    coded heroSection sceleton

---
+++ src/components/heroSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Container from './helperComponents/container';
+import Centrifier from './helperComponents/centrifier';
+
+const HeroSection = () => {
+  <div className="hero-section">
+    <Container>
+      <Centrifier>hi</Centrifier>
+    </Container>
+  </div>;
+};
+
+export default HeroSection;
