52ce9ec91e86d9cbfeb04cc341043996405b6cd7
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 19:31:20 +0100

    added basic footer

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './footer.sass';
+
+const Footer = () => <div className="footer-section" />;
+
+export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Header from './header';
+import Footer from './footer';
 import './layout.sass';
 
 const Layout = ({ children }) => (
   <>
     <Header siteTitle="Pebble" />
-    <div>{children}</div>
+    {children}
+    <Footer />
   </>
 );
 
