72344045cfe25645c72d9acfbe14490b3706129b
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 16:17:40 +0100

    featureSection now in index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import HeroSection from '../components/heroSection';
 import ProblemSection from '../components/problemSection';
+import FeatureSection from '../components/featureSection';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HeroSection />
     <ProblemSection />
+    <FeatureSection />
   </Layout>
 );
 
