957db73a2ff1f371a1893fa4f32f6be1923d6c4f
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 19:47:07 +0100

    added centrifiers around feature grid and pricing plans box

--- src/components/featureSection.js
+++ src/components/featureSection.js
@@ -10,134 +10,136 @@ const FeatureSection = () => (
   <div className="feature-section">
     <Container>
       <p className="feature-section__heading">Features</p>
-      <div className="feature-section__card-grid">
-        <Card type="feature">
-          <p className="card__heading">Construction Tech</p>
-          <p className="card__main-emoji">
-            <span role="img" aria-label="construction emoji">
-              &#x1F3D7;
-            </span>
-          </p>
-          <p className="card__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis
-          </p>
-          <Centrifier>
-            <p className="card__read-more">
-              <span role="img" aria-label="pointing finger">
-                &#x1F449;
+      <Centrifier>
+        <div className="feature-section__card-grid">
+          <Card type="feature">
+            <p className="card__heading">Construction Tech</p>
+            <p className="card__main-emoji">
+              <span role="img" aria-label="construction emoji">
+                &#x1F3D7;
               </span>
-              Read more
             </p>
-          </Centrifier>
-        </Card>
-        <Card type="feature">
-          <p className="card__heading">Nanotechnology</p>
-          <p className="card__main-emoji">
-            <span role="img" aria-label="microscope emoji">
-              &#x1F52C;
-            </span>
-          </p>
-          <p className="card__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis
-          </p>
-          <Centrifier>
-            <p className="card__read-more">
-              <span role="img" aria-label="pointing finger">
-                &#x1F449;
+            <p className="card__text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis
+            </p>
+            <Centrifier>
+              <p className="card__read-more">
+                <span role="img" aria-label="pointing finger">
+                  &#x1F449;
+                </span>
+                Read more
+              </p>
+            </Centrifier>
+          </Card>
+          <Card type="feature">
+            <p className="card__heading">Nanotechnology</p>
+            <p className="card__main-emoji">
+              <span role="img" aria-label="microscope emoji">
+                &#x1F52C;
               </span>
-              Read more
             </p>
-          </Centrifier>
-        </Card>
-        <Card type="feature">
-          <p className="card__heading">Robotics</p>
-          <p className="card__main-emoji">
-            <span role="img" aria-label="robot emoji">
-              &#x1F916;
-            </span>
-          </p>
-          <p className="card__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis
-          </p>
-          <Centrifier>
-            <p className="card__read-more">
-              <span role="img" aria-label="pointing finger">
-                &#x1F449;
+            <p className="card__text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis
+            </p>
+            <Centrifier>
+              <p className="card__read-more">
+                <span role="img" aria-label="pointing finger">
+                  &#x1F449;
+                </span>
+                Read more
+              </p>
+            </Centrifier>
+          </Card>
+          <Card type="feature">
+            <p className="card__heading">Robotics</p>
+            <p className="card__main-emoji">
+              <span role="img" aria-label="robot emoji">
+                &#x1F916;
               </span>
-              Read more
             </p>
-          </Centrifier>
-        </Card>
-        <Card type="feature">
-          <p className="card__heading">Bioengineering</p>
-          <p className="card__main-emoji">
-            <span role="img" aria-label="DNA emoji">
-              &#x1F9EC;
-            </span>
-          </p>
-          <p className="card__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis
-          </p>
-          <Centrifier>
-            <p className="card__read-more">
-              <span role="img" aria-label="pointing finger">
-                &#x1F449;
+            <p className="card__text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis
+            </p>
+            <Centrifier>
+              <p className="card__read-more">
+                <span role="img" aria-label="pointing finger">
+                  &#x1F449;
+                </span>
+                Read more
+              </p>
+            </Centrifier>
+          </Card>
+          <Card type="feature">
+            <p className="card__heading">Bioengineering</p>
+            <p className="card__main-emoji">
+              <span role="img" aria-label="DNA emoji">
+                &#x1F9EC;
               </span>
-              Read more
             </p>
-          </Centrifier>
-        </Card>
-        <Card type="feature">
-          <p className="card__heading">Cybersecurity</p>
-          <p className="card__main-emoji">
-            <span role="img" aria-label="police car emoji">
-              &#x1F693;
-            </span>
-          </p>
-          <p className="card__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis
-          </p>
-          <Centrifier>
-            <p className="card__read-more">
-              <span role="img" aria-label="pointing finger">
-                &#x1F449;
+            <p className="card__text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis
+            </p>
+            <Centrifier>
+              <p className="card__read-more">
+                <span role="img" aria-label="pointing finger">
+                  &#x1F449;
+                </span>
+                Read more
+              </p>
+            </Centrifier>
+          </Card>
+          <Card type="feature">
+            <p className="card__heading">Cybersecurity</p>
+            <p className="card__main-emoji">
+              <span role="img" aria-label="police car emoji">
+                &#x1F693;
               </span>
-              Read more
             </p>
-          </Centrifier>
-        </Card>
-        <Card type="feature">
-          <p className="card__heading">Neuroscience</p>
-          <p className="card__main-emoji">
-            <span role="img" aria-label="brain emoji">
-              &#x1F9E0;
-            </span>
-          </p>
-          <p className="card__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis
-          </p>
-          <Centrifier>
-            <p className="card__read-more">
-              <span role="img" aria-label="pointing finger">
-                &#x1F449;
+            <p className="card__text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis
+            </p>
+            <Centrifier>
+              <p className="card__read-more">
+                <span role="img" aria-label="pointing finger">
+                  &#x1F449;
+                </span>
+                Read more
+              </p>
+            </Centrifier>
+          </Card>
+          <Card type="feature">
+            <p className="card__heading">Neuroscience</p>
+            <p className="card__main-emoji">
+              <span role="img" aria-label="brain emoji">
+                &#x1F9E0;
               </span>
-              Read more
             </p>
-          </Centrifier>
-        </Card>
-      </div>
+            <p className="card__text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis
+            </p>
+            <Centrifier>
+              <p className="card__read-more">
+                <span role="img" aria-label="pointing finger">
+                  &#x1F449;
+                </span>
+                Read more
+              </p>
+            </Centrifier>
+          </Card>
+        </div>
+      </Centrifier>
     </Container>
   </div>
 );
--- src/components/pricingSection.js
+++ src/components/pricingSection.js
@@ -4,74 +4,89 @@ import Container from './helperComponents/container';
 import Card from './card';
 
 import './pricingSection.sass';
+import Centrifier from './helperComponents/centrifier';
 
 const PricingSection = () => (
   <div className="pricing-section">
     <Container>
       <p className="pricing-section__heading">Pricing</p>
-      <div className="pricing-section__plan-box">
-        <Card type="pricing">
-          <p className="card--pricing__plan-name">
-            Basic
-            <span role="img" aria-label="basic plan emoji">
-              &#x1F41F;
-            </span>
-          </p>
-          <p className="card--pricing__plan-price">9$</p>
-          <p className="card--pricing__plan-price--info">/user/month</p>
-          <p className="card--pricing__plan-feature">
-            fascinating artificial intelligence
-          </p>
-          <p className="card--pricing__plan-feature">
-            sublime construction tech
-          </p>
-          <p className="card--pricing__plan-feature">astonishing speed</p>
-          <p className="card--pricing__plan-feature">
-            magnificent motherboards
-          </p>
-          <p className="card--pricing__plan-feature">
-            state-of-the-art bioengineering
-          </p>
-          <p className="card--pricing__plan-feature">dazzling devOps</p>
-        </Card>
-        <Card type="pricing">
-          <p className="card--pricing__plan-name">
-            Pro
-            <span role="img" aria-label="pro plan emoji">
-              &#x1F42C;
-            </span>
-          </p>
-          <p className="card--pricing__plan-price">20$</p>
-          <p className="card--pricing__plan-price--info">/user/month</p>
-          <p className="card--pricing__plan-feature">all the basic features</p>
-          <p className="card--pricing__plan-feature">+</p>
-          <p className="card--pricing__plan-feature">cool blockchain thingy</p>
-          <p className="card--pricing__plan-feature">some machine learning</p>
-          <p className="card--pricing__plan-feature">crazy nanotechnology</p>
-          <p className="card--pricing__plan-feature">more spectacular things</p>
-        </Card>
-        <Card type="pricing">
-          <p className="card--pricing__plan-name">
-            Enterprise
-            <span
-              className="whale-emoji"
-              role="img"
-              aria-label="enterprise plan emoji"
-            >
-              &#x1F433;
-            </span>
-          </p>
-          <p className="card--pricing__plan-contact">Contact us</p>
-          <p className="card--pricing__plan-feature">
-            all the basic and pro features
-          </p>
-          <p className="card--pricing__plan-feature">+</p>
-          <p className="card--pricing__plan-feature">more bling bling</p>
-          <p className="card--pricing__plan-feature">crazy good 24/7 support</p>
-          <p className="card--pricing__plan-feature">top-notch cybersecurity</p>
-          <p className="card--pricing__plan-feature">robots with sick moves</p>
-        </Card>
-      </div>
+      <Centrifier>
+        <div className="pricing-section__plan-box">
+          <Card type="pricing">
+            <p className="card--pricing__plan-name">
+              Basic
+              <span role="img" aria-label="basic plan emoji">
+                &#x1F41F;
+              </span>
+            </p>
+            <p className="card--pricing__plan-price">9$</p>
+            <p className="card--pricing__plan-price--info">/user/month</p>
+            <p className="card--pricing__plan-feature">
+              fascinating artificial intelligence
+            </p>
+            <p className="card--pricing__plan-feature">
+              sublime construction tech
+            </p>
+            <p className="card--pricing__plan-feature">astonishing speed</p>
+            <p className="card--pricing__plan-feature">
+              magnificent motherboards
+            </p>
+            <p className="card--pricing__plan-feature">
+              state-of-the-art bioengineering
+            </p>
+            <p className="card--pricing__plan-feature">dazzling devOps</p>
+          </Card>
+          <Card type="pricing">
+            <p className="card--pricing__plan-name">
+              Pro
+              <span role="img" aria-label="pro plan emoji">
+                &#x1F42C;
+              </span>
+            </p>
+            <p className="card--pricing__plan-price">20$</p>
+            <p className="card--pricing__plan-price--info">/user/month</p>
+            <p className="card--pricing__plan-feature">
+              all the basic features
+            </p>
+            <p className="card--pricing__plan-feature">+</p>
+            <p className="card--pricing__plan-feature">
+              cool blockchain thingy
+            </p>
+            <p className="card--pricing__plan-feature">some machine learning</p>
+            <p className="card--pricing__plan-feature">crazy nanotechnology</p>
+            <p className="card--pricing__plan-feature">
+              more spectacular things
+            </p>
+          </Card>
+          <Card type="pricing">
+            <p className="card--pricing__plan-name">
+              Enterprise
+              <span
+                className="whale-emoji"
+                role="img"
+                aria-label="enterprise plan emoji"
+              >
+                &#x1F433;
+              </span>
+            </p>
+            <p className="card--pricing__plan-contact">Contact us</p>
+            <p className="card--pricing__plan-feature">
+              all the basic and pro features
+            </p>
+            <p className="card--pricing__plan-feature">+</p>
+            <p className="card--pricing__plan-feature">more bling bling</p>
+            <p className="card--pricing__plan-feature">
+              crazy good 24/7 support
+            </p>
+            <p className="card--pricing__plan-feature">
+              top-notch cybersecurity
+            </p>
+            <p className="card--pricing__plan-feature">
+              robots with sick moves
+            </p>
+          </Card>
+        </div>
+      </Centrifier>
     </Container>
   </div>
 );
