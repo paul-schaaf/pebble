d4016409012af29f052263b8e7efe0caeac30b94
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 13:25:25 +0100

    imported heroSection.sass into js file

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Container from './helperComponents/container';
 import Centrifier from './helperComponents/centrifier';
 
+import './heroSection.sass';
+
 const HeroSection = () => (
   <div className="hero-section">
     <Container>
