789f9e7dffdb85f1d2f702d5f1bdfe555b5d5fb7
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 16:16:54 +0100

    created featureSection skeleton

---
+++ src/components/featureSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Container from './helperComponents/container';
+import Centrifier from './helperComponents/centrifier';
+
+import './featureSection.sass';
+
+const FeatureSection = () => (
+  <div className="feature-section">
+    <Container>
+      <p>Features</p>
+    </Container>
+  </div>
+);
+
+export default FeatureSection;
