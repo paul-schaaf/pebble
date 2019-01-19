bcdb3eab66fbad78449d41055531d82f06919a5e
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 01:07:54 +0100

    layout now uses sass

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Header from './header';
-import './layout.css';
+import './layout.sass';
 
 const Layout = ({ children }) => (
   <>
