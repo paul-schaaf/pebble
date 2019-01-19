863ca7abbec6a6ad2b2f451a872fcc2b3aeefcc3
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 17:15:48 +0100

    added pricing cards

--- src/components/pricingSection.js
+++ src/components/pricingSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Container from './helperComponents/container';
+import Card from './card';
 
 import './pricingSection.sass';
 
@@ -8,6 +9,11 @@ const PricingSection = () => (
   <div className="pricing-section">
     <Container>
       <p className="pricing-section__heading">Pricing</p>
+      <div className="pricing-section__plan-box">
+        <Card type="pricing">Basic</Card>
+        <Card type="pricing">Pro</Card>
+        <Card type="pricing">Enterprise</Card>
+      </div>
     </Container>
   </div>
 );
