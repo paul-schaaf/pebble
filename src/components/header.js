f3568fbe89f3b654f19b0501d7e78f1fd6342ccc
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 19:34:11 +0100

    turned header into class component

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,27 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 
 import Container from './helperComponents/container';
 
 import './header.sass';
 
-const Header = ({ siteTitle }) => (
-  <div className="navbar-box">
-    <Container>
-      <p className="title">{siteTitle}</p>
-      <div className="link-box">
-        <p>Why Pebble?</p>
-        <p>Features</p>
-        <p>Pricing</p>
-        <p>Praise</p>
+class Header extends Component {
+  render() {
+    return (
+      <div className="navbar-box">
+        <Container>
+          <p className="title">{this.props.siteTitle}</p>
+          <div className="link-box">
+            <p>Why Pebble?</p>
+            <p>Features</p>
+            <p>Pricing</p>
+            <p>Praise</p>
+          </div>
+        </Container>
       </div>
-    </Container>
-  </div>
-);
+    );
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
