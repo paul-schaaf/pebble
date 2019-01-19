70a0ba969de6d883c7ff6f1853c50da309004f6c
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 14:13:37 +0100

    included ProblemSection in index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import HeroSection from '../components/heroSection';
+import ProblemSection from '../components/problemSection';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HeroSection />
+    <ProblemSection />
   </Layout>
 );
 
