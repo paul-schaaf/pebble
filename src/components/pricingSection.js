cec451e45c73d5d98f5afb198dca4df2fef7381b
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 17:54:55 +0100

    completed pricing cards

--- src/components/pricingSection.js
+++ src/components/pricingSection.js
@@ -19,6 +19,20 @@ const PricingSection = () => (
           </p>
           <p className="card--pricing__plan-price">9$</p>
           <p className="card--pricing__plan-price--info">/user/month</p>
+          <p className="card--pricing__plan-feature">
+            fascinating artificial intelligence
+          </p>
+          <p className="card--pricing__plan-feature">
+            sublime construction tech
+          </p>
+          <p className="card--pricing__plan-feature">astonishing speed</p>
+          <p className="card--pricing__plan-feature">
+            magnificent motherboards
+          </p>
+          <p className="card--pricing__plan-feature">
+            state-of-the-art bioengineering
+          </p>
+          <p className="card--pricing__plan-feature">dazzling devOps</p>
         </Card>
         <Card type="pricing">
           <p className="card--pricing__plan-name">
@@ -29,6 +43,12 @@ const PricingSection = () => (
           </p>
           <p className="card--pricing__plan-price">20$</p>
           <p className="card--pricing__plan-price--info">/user/month</p>
+          <p className="card--pricing__plan-feature">all the basic features</p>
+          <p className="card--pricing__plan-feature">+</p>
+          <p className="card--pricing__plan-feature">cool blockchain thingy</p>
+          <p className="card--pricing__plan-feature">some machine learning</p>
+          <p className="card--pricing__plan-feature">crazy nanotechnology</p>
+          <p className="card--pricing__plan-feature">more spectacular things</p>
         </Card>
         <Card type="pricing">
           <p className="card--pricing__plan-name">
@@ -42,6 +62,14 @@ const PricingSection = () => (
             </span>
           </p>
           <p className="card--pricing__plan-contact">Contact us</p>
+          <p className="card--pricing__plan-feature">
+            all the basic and pro features
+          </p>
+          <p className="card--pricing__plan-feature">+</p>
+          <p className="card--pricing__plan-feature">more bling bling</p>
+          <p className="card--pricing__plan-feature">crazy good 24/7 support</p>
+          <p className="card--pricing__plan-feature">top-notch cybersecurity</p>
+          <p className="card--pricing__plan-feature">robots with sick moves</p>
         </Card>
       </div>
     </Container>
