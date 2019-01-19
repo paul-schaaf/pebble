fa6c46f3f02785c7088396bdab91ef78f6407f26
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 13:24:21 +0100

    included heroSection in index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import HeroSection from '../components/heroSection';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <HeroSection />
   </Layout>
 );
 
