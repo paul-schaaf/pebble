553a6a592076a87c2c4a085d0e1eef520d294546
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 18:55:43 +0100

    finished praise cards

--- src/components/praiseSection.js
+++ src/components/praiseSection.js
@@ -12,21 +12,117 @@ const PraiseSection = () => (
     </Container>
     <div className="praise-section__scroll-box">
       <div className="praise-section__scroll-box__card-box--one">
-        <Card type="praise">hi</Card>
-        <Card type="praise">hi</Card>
-        <Card type="praise">hi</Card>
-        <Card type="praise">hi</Card>
-        <Card type="praise">hi</Card>
-        <Card type="praise">hi</Card>
+        <Card type="praise">
+          <p className="card--praise__praise-emoji">
+            <span role="img" aria-label="praise emoji">
+              👩‍🦳
+            </span>
+          </p>
+          <p className="card--praise__praise-text">Amazing tool!</p>
+          <p className="card--praise__praise-author">- @Jayqueline</p>
+        </Card>
+        <Card type="praise">
+          <p className="card--praise__praise-emoji">
+            <span role="img" aria-label="praise emoji">
+              👩‍🦳
+            </span>
+          </p>
+          <p className="card--praise__praise-text">Amazing tool!</p>
+          <p className="card--praise__praise-author">- @Jayqueline</p>
+        </Card>
+        <Card type="praise">
+          <p className="card--praise__praise-emoji">
+            <span role="img" aria-label="praise emoji">
+              👩‍🦳
+            </span>
+          </p>
+          <p className="card--praise__praise-text">Amazing tool!</p>
+          <p className="card--praise__praise-author">- @Jayqueline</p>
+        </Card>
+        <Card type="praise">
+          <p className="card--praise__praise-emoji">
+            <span role="img" aria-label="praise emoji">
+              👩‍🦳
+            </span>
+          </p>
+          <p className="card--praise__praise-text">Amazing tool!</p>
+          <p className="card--praise__praise-author">- @Jayqueline</p>
+        </Card>
+        <Card type="praise">
+          <p className="card--praise__praise-emoji">
+            <span role="img" aria-label="praise emoji">
+              👩‍🦳
+            </span>
+          </p>
+          <p className="card--praise__praise-text">Amazing tool!</p>
+          <p className="card--praise__praise-author">- @Jayqueline</p>
+        </Card>
+        <Card type="praise">
+          <p className="card--praise__praise-emoji">
+            <span role="img" aria-label="praise emoji">
+              👩‍🦳
+            </span>
+          </p>
+          <p className="card--praise__praise-text">Amazing tool!</p>
+          <p className="card--praise__praise-author">- @Jayqueline</p>
+        </Card>
         <div className="praise-section__scroll-box__card-box--one__placeholder" />
       </div>
       <div className="praise-section__scroll-box__card-box">
-        <Card type="praise">hi</Card>
-        <Card type="praise">hi</Card>
-        <Card type="praise">hi</Card>
-        <Card type="praise">hi</Card>
-        <Card type="praise">hi</Card>
-        <Card type="praise">hi</Card>
+        <Card type="praise">
+          <p className="card--praise__praise-emoji">
+            <span role="img" aria-label="praise emoji">
+              👩‍🦳
+            </span>
+          </p>
+          <p className="card--praise__praise-text">Amazing tool!</p>
+          <p className="card--praise__praise-author">- @Jayqueline</p>
+        </Card>
+        <Card type="praise">
+          <p className="card--praise__praise-emoji">
+            <span role="img" aria-label="praise emoji">
+              👩‍🦳
+            </span>
+          </p>
+          <p className="card--praise__praise-text">Amazing tool!</p>
+          <p className="card--praise__praise-author">- @Jayqueline</p>
+        </Card>
+        <Card type="praise">
+          <p className="card--praise__praise-emoji">
+            <span role="img" aria-label="praise emoji">
+              👩‍🦳
+            </span>
+          </p>
+          <p className="card--praise__praise-text">Amazing tool!</p>
+          <p className="card--praise__praise-author">- @Jayqueline</p>
+        </Card>
+        <Card type="praise">
+          <p className="card--praise__praise-emoji">
+            <span role="img" aria-label="praise emoji">
+              👩‍🦳
+            </span>
+          </p>
+          <p className="card--praise__praise-text">Amazing tool!</p>
+          <p className="card--praise__praise-author">- @Jayqueline</p>
+        </Card>
+        <Card type="praise">
+          <p className="card--praise__praise-emoji">
+            <span role="img" aria-label="praise emoji">
+              👩‍🦳
+            </span>
+          </p>
+          <p className="card--praise__praise-text">Amazing tool!</p>
+          <p className="card--praise__praise-author">- @Jayqueline</p>
+        </Card>
+        <Card type="praise">
+          <p className="card--praise__praise-emoji">
+            <span role="img" aria-label="praise emoji">
+              👩‍🦳
+            </span>
+          </p>
+          <p className="card--praise__praise-text">Amazing tool!</p>
+          <p className="card--praise__praise-author">- @Jayqueline</p>
+        </Card>
       </div>
     </div>
   </div>
