5fb62b9e9c39b61f84c5d09beb50de6942f2a2ad
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-19 17:04:16 +0100

    created pricingSection and its header

---
+++ src/components/pricingSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Container from './helperComponents/container';
+
+import './pricingSection.sass';
+
+const PricingSection = () => (
+  <div className="pricing-section">
+    <Container>
+      <p className="pricing-section__heading">Pricing</p>
+    </Container>
+  </div>
+);
+
+export default PricingSection;
