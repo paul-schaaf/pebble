e964aa116470cc01335ae172d70284aa7424526f
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-20 17:45:44 +0100

    footer is now responsive for all screens

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,39 +6,37 @@ import './footer.sass';
 
 const Footer = () => (
   <div className="footer-section">
-    <Centrifier>
-      <div className="footer-section__box">
-        <p className="footer-section__box__heading">Pebble</p>
-        <p className="footer-section__box__info-text">
-          Solving key issues
-          <br />
-          the stone industry is
-          <br />
-          facing today
-        </p>
-      </div>
-      <div className="footer-section__box">
-        <p className="footer-section__box__heading">Links</p>
-        <p className="footer-section__box__footer-link">Download for Mac</p>
-        <p className="footer-section__box__footer-link">Download for Windows</p>
-        <p className="footer-section__box__footer-link">Download for iOS</p>
-        <p className="footer-section__box__footer-link">Download for Android</p>
-      </div>
-      <div className="footer-section__box">
-        <p className="footer-section__box__heading">Company</p>
-        <p className="footer-section__box__footer-link">About</p>
-        <p className="footer-section__box__footer-link">Career</p>
-        <p className="footer-section__box__footer-link">Media Kit</p>
-        <p className="footer-section__box__footer-link">Blog</p>
-      </div>
-      <div className="footer-section__box footer-section__box--last">
-        <p className="footer-section__box__heading">Contact</p>
-        <p className="footer-section__box__footer-link">Livechat</p>
-        <p className="footer-section__box__footer-link">Twitter</p>
-        <p className="footer-section__box__footer-link">Facebook</p>
-        <p className="footer-section__box__footer-link">Email</p>
-      </div>
-    </Centrifier>
+    <div className="footer-section__box">
+      <p className="footer-section__box__heading">Pebble</p>
+      <p className="footer-section__box__info-text">
+        Solving key issues
+        <br />
+        the stone industry is
+        <br />
+        facing today
+      </p>
+    </div>
+    <div className="footer-section__box">
+      <p className="footer-section__box__heading">Links</p>
+      <p className="footer-section__box__footer-link">Download for Mac</p>
+      <p className="footer-section__box__footer-link">Download for Windows</p>
+      <p className="footer-section__box__footer-link">Download for iOS</p>
+      <p className="footer-section__box__footer-link">Download for Android</p>
+    </div>
+    <div className="footer-section__box">
+      <p className="footer-section__box__heading">Company</p>
+      <p className="footer-section__box__footer-link">About</p>
+      <p className="footer-section__box__footer-link">Career</p>
+      <p className="footer-section__box__footer-link">Media Kit</p>
+      <p className="footer-section__box__footer-link">Blog</p>
+    </div>
+    <div className="footer-section__box footer-section__box--last">
+      <p className="footer-section__box__heading">Contact</p>
+      <p className="footer-section__box__footer-link">Livechat</p>
+      <p className="footer-section__box__footer-link">Twitter</p>
+      <p className="footer-section__box__footer-link">Facebook</p>
+      <p className="footer-section__box__footer-link">Email</p>
+    </div>
   </div>
 );
 
