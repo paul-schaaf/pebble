73194b9e998c24ff14f71b2dcf31ace98ff1f31d
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-21 14:58:09 +0100

    created praise card content component

--- src/components/praiseSection.js
+++ src/components/praiseSection.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Container from './helperComponents/container';
 import Card from './card';
 
 import './praiseSection.sass';
+
+const PraiseCardContent = ({ emoji, text, author }) => (
+  <Fragment>
+    <p className="card--praise__praise-emoji">
+      <span role="img" aria-label="praise emoji">
+        {emoji}
+      </span>
+    </p>
+    <p className="card--praise__praise-text">{text}</p>
+    <p className="card--praise__praise-author">{`- @${author}`}</p>
+  </Fragment>
+);
 
 const PraiseSection = () => (
   <div className="praise-section">
@@ -13,151 +25,83 @@ const PraiseSection = () => (
     <div className="praise-section__scroll-box">
       <div className="praise-section__scroll-box__card-box--one">
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              👩‍🦳
-            </span>
-          </p>
-          <p className="card--praise__praise-text">Amazing tool!</p>
-          <p className="card--praise__praise-author">- @Jayqueline</p>
+          <PraiseCardContent
+            emoji="👩‍🦳"
+            text="Amazing tool!"
+            author="Jayqueline"
+          />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              🤖
-            </span>
-          </p>
-          <p className="card--praise__praise-text">Beep Boop.</p>
-          <p className="card--praise__praise-author">- @Mr. Robot</p>
+          <PraiseCardContent emoji="🤖" text="Beep Boop." author="Mr.Robot" />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              👨‍🦳
-            </span>
-          </p>
-          <p className="card--praise__praise-text">Spectacular!</p>
-          <p className="card--praise__praise-author">- @Bob</p>
+          <PraiseCardContent emoji="👨‍🦳" text="Spectacular!" author="Bob" />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              👵🏻
-            </span>
-          </p>
-          <p className="card--praise__praise-text">I love this thing!</p>
-          <p className="card--praise__praise-author">- @grandma</p>
+          <PraiseCardContent
+            emoji="👵🏻"
+            text="I love this thing!!"
+            author="grandma"
+          />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              👩🏾‍🦱
-            </span>
-          </p>
-          <p className="card--praise__praise-text">Fantastic Features!</p>
-          <p className="card--praise__praise-author">- @annie</p>
+          <PraiseCardContent
+            emoji="👩🏾‍🦱"
+            text="Fantastic Features!"
+            author="annie"
+          />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              🧟
-            </span>
-          </p>
-          <p className="card--praise__praise-text">Nice work, yummy!</p>
-          <p className="card--praise__praise-author">- @theFriendlyZombie</p>
+          <PraiseCardContent
+            emoji="🧟"
+            text="Nice work, yummy!"
+            author="theFriendlyZombie"
+          />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              👩‍🎨
-            </span>
-          </p>
-          <p className="card--praise__praise-text">Beautiful!</p>
-          <p className="card--praise__praise-author">- @emilia</p>
+          <PraiseCardContent emoji="👩‍🎨" text="Beautiful!" author="emilia" />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              👨‍🚀
-            </span>
-          </p>
-          <p className="card--praise__praise-text">Five stars!</p>
-          <p className="card--praise__praise-author">- @oscar</p>
+          <PraiseCardContent emoji="👨‍🚀" text="Five stars!" author="oscar" />
         </Card>
         <div className="praise-section__scroll-box__card-box--one__placeholder" />
       </div>
       <div className="praise-section__scroll-box__card-box">
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              👩🏻
-            </span>
-          </p>
-          <p className="card--praise__praise-text">So great and simple!</p>
-          <p className="card--praise__praise-author">- @Dnice</p>
+          <PraiseCardContent
+            emoji="👩🏻"
+            text="So great and simple!"
+            author="Dnice"
+          />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              🎅
-            </span>
-          </p>
-          <p className="card--praise__praise-text">Pebble is the bomb!</p>
-          <p className="card--praise__praise-author">- @Santa</p>
+          <PraiseCardContent
+            emoji="🎅"
+            text="Pebble is the bomb!"
+            author="Santa"
+          />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              👨🏾
-            </span>
-          </p>
-          <p className="card--praise__praise-text">Awesome stuff!</p>
-          <p className="card--praise__praise-author">- @Jack</p>
+          <PraiseCardContent emoji="👨🏾" text="Awesome stuff!" author="Jack" />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              👩🏻‍🦰
-            </span>
-          </p>
-          <p className="card--praise__praise-text">I've fallen in love!</p>
-          <p className="card--praise__praise-author">- @hannah</p>
+          <PraiseCardContent
+            emoji="👩🏻‍🦰"
+            text="I've fallen in love!"
+            author="hannah"
+          />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              👨‍🔬
-            </span>
-          </p>
-          <p className="card--praise__praise-text">Just Sublime!</p>
-          <p className="card--praise__praise-author">- @Lance</p>
+          <PraiseCardContent emoji="👨‍🔬" text="Just Sublime!" author="Lance" />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              👨🏾‍💻
-            </span>
-          </p>
-          <p className="card--praise__praise-text">Magnificent!</p>
-          <p className="card--praise__praise-author">- @henry</p>
+          <PraiseCardContent emoji="👨🏾‍💻" text="Magnificent" author="henry" />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              👸🏼
-            </span>
-          </p>
-          <p className="card--praise__praise-text">Yes, yes, yes!</p>
-          <p className="card--praise__praise-author">- @sissi</p>
+          <PraiseCardContent emoji="👸🏼" text="Yes, yes, yes!" author="sissi" />
         </Card>
         <Card type="praise">
-          <p className="card--praise__praise-emoji">
-            <span role="img" aria-label="praise emoji">
-              👮🏿
-            </span>
-          </p>
-          <p className="card--praise__praise-text">Very secure!</p>
-          <p className="card--praise__praise-author">- @richard</p>
+          <PraiseCardContent emoji="👮🏿" text="Very secure!" author="richard" />
         </Card>
       </div>
     </div>
