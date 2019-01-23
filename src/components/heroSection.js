9322bc9c531ab05fba5b489fe343b4f30f73c9d2
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-23 19:15:51 +0100

    added transition to heroHeading

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Transition } from 'react-spring';
 import Img from 'gatsby-image';
 
 import Container from './helperComponents/container';
@@ -7,15 +8,46 @@ import Centrifier from './helperComponents/centrifier';
 import './heroSection.sass';
 
 class HeroSection extends Component {
+  state = {
+    show: false,
+  };
+
+  componentDidMount = () => this.setState({ show: true });
+
   render() {
     return (
       <div className="hero-section">
         <Container>
           <Centrifier>
             <div className="hero-section__content">
-              <p className="hero-section__content__heading">Stones</p>
+              <Transition
+                items={this.state.show}
+                from={{ opacity: 0 }}
+                enter={{ opacity: 1 }}
+                leave={{ opacity: 0 }}
+                config={{ duration: 2000 }}
+              >
+                {show =>
+                  show &&
+                  (props => (
+                    <Fragment>
+                      <p
+                        style={props}
+                        className="hero-section__content__heading"
+                      >
+                        Stones
+                      </p>
+                      <p
+                        style={props}
+                        className="hero-section__content__heading"
+                      >
+                        Redefined
+                      </p>
+                    </Fragment>
+                  ))
+                }
+              </Transition>
 
-              <p className="hero-section__content__heading">Redefined</p>
               <Centrifier>
                 <div className="hero-section__content__text">
                   <span aria-label="pointing finger" role="img">
