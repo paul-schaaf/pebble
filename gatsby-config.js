a9a30747fc21fec7cce1afcb9578d06588fee316
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-23 16:02:15 +0100

    added better favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Pebble`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Pebble tackles key issues the stone industry is facing`,
     author: `@paulschaaf`,
   },
   plugins: [
