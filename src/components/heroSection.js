2c871e147ce14feff0fdda4bd2da217fbd32b9fd
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-23 18:59:23 +0100

    turned hero section into class

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Img from 'gatsby-image';
 
 import Container from './helperComponents/container';
@@ -6,29 +6,33 @@ import Centrifier from './helperComponents/centrifier';
 
 import './heroSection.sass';
 
-const HeroSection = ({ fingerEmoji }) => (
-  <div className="hero-section">
-    <Container>
-      <Centrifier>
-        <div className="hero-section__content">
-          <p className="hero-section__content__heading">Stones</p>
-
-          <p className="hero-section__content__heading">Redefined</p>
+class HeroSection extends Component {
+  render() {
+    return (
+      <div className="hero-section">
+        <Container>
           <Centrifier>
-            <div className="hero-section__content__text">
-              <span aria-label="pointing finger" role="img">
-                <Img
-                  alt="pointing finger"
-                  fluid={fingerEmoji.childImageSharp.fluid}
-                />
-              </span>
-              Sign up for trial
+            <div className="hero-section__content">
+              <p className="hero-section__content__heading">Stones</p>
+
+              <p className="hero-section__content__heading">Redefined</p>
+              <Centrifier>
+                <div className="hero-section__content__text">
+                  <span aria-label="pointing finger" role="img">
+                    <Img
+                      alt="pointing finger"
+                      fluid={this.props.fingerEmoji.childImageSharp.fluid}
+                    />
+                  </span>
+                  Sign up for trial
+                </div>
+              </Centrifier>
             </div>
           </Centrifier>
-        </div>
-      </Centrifier>
-    </Container>
-  </div>
-);
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default HeroSection;
