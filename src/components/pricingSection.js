0b19e63adc778ffa7d075a960e13f1d0ee638e52
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-23 18:36:30 +0100

    swaped pricingCardContent with previous jsx

--- src/components/pricingSection.js
+++ src/components/pricingSection.js
@@ -18,7 +18,12 @@ const PricingCardContent = ({ name, emoji, alt, price, features }) => (
           name === 'Pro' ? 'dolphin' : name === 'Enterprise' ? 'whale' : null
         }
       >
-        <Img alt={alt} fixed={emoji.childImageSharp.fixed} />
+        {name === 'Enterprise' && (
+          <Img alt={alt} fluid={emoji.childImageSharp.fluid} />
+        )}
+        {name !== 'Enterprise' && (
+          <Img alt={alt} fixed={emoji.childImageSharp.fixed} />
+        )}
       </span>
     </div>
 
@@ -75,82 +80,51 @@ const PricingSection = ({ smallFishEmoji, mediumFishEmoji, bigFishEmoji }) => (
       <Centrifier>
         <div className="pricing-section__plan-box">
           <Card type="pricing">
-            <div className="card--pricing__plan-name">
-              Basic
-              <span>
-                <Img
-                  alt="basic plan emoji"
-                  fixed={smallFishEmoji.childImageSharp.fixed}
-                />
-              </span>
-            </div>
-            <p className="card--pricing__plan-price">9$</p>
-            <p className="card--pricing__plan-price--info">/user/month</p>
-            <p className="card--pricing__plan-feature">
-              fascinating artificial intelligence
-            </p>
-            <p className="card--pricing__plan-feature">
-              sublime construction tech
-            </p>
-            <p className="card--pricing__plan-feature">astonishing speed</p>
-            <p className="card--pricing__plan-feature">
-              magnificent motherboards
-            </p>
-            <p className="card--pricing__plan-feature">
-              state-of-the-art bioengineering
-            </p>
-            <p className="card--pricing__plan-feature">dazzling devOps</p>
+            <PricingCardContent
+              name="Basic"
+              emoji={smallFishEmoji}
+              alt="basic plan emoji"
+              price={9}
+              features={[
+                'fascinating artificial intelligence',
+                'sublime construction tech',
+                'astonishing speed',
+                'magnificent motherboards',
+                'state-of-the-art bioengineering',
+                'dazzling devOps',
+              ]}
+            />
           </Card>
           <Card type="pricing">
-            <div className="card--pricing__plan-name">
-              Pro
-              <span className="dolphin">
-                <Img
-                  alt="pro plan emoji"
-                  fixed={mediumFishEmoji.childImageSharp.fixed}
-                />
-              </span>
-            </div>
-            <p className="card--pricing__plan-price">20$</p>
-            <p className="card--pricing__plan-price--info">/user/month</p>
-            <p className="card--pricing__plan-feature">
-              all the basic features
-            </p>
-            <p className="card--pricing__plan-feature">+</p>
-            <p className="card--pricing__plan-feature">
-              cool blockchain thingy
-            </p>
-            <p className="card--pricing__plan-feature">some machine learning</p>
-            <p className="card--pricing__plan-feature">crazy nanotechnology</p>
-            <p className="card--pricing__plan-feature">
-              more spectacular things
-            </p>
+            <PricingCardContent
+              name="Pro"
+              emoji={mediumFishEmoji}
+              alt="pro plan emoji"
+              price={20}
+              features={[
+                'all the basic features',
+                '+',
+                'cool blockchain thingy',
+                'some machine learning',
+                'crazy nanotechnology',
+                'more spectacular things',
+              ]}
+            />
           </Card>
           <Card type="pricing">
-            <div className="card--pricing__plan-name">
-              Enterprise
-              <span className="whale">
-                <Img
-                  alt="enterprise plan emoji"
-                  fluid={bigFishEmoji.childImageSharp.fluid}
-                />
-              </span>
-            </div>
-            <p className="card--pricing__plan-contact">Contact us</p>
-            <p className="card--pricing__plan-feature">
-              all the basic and pro features
-            </p>
-            <p className="card--pricing__plan-feature">+</p>
-            <p className="card--pricing__plan-feature">more bling bling</p>
-            <p className="card--pricing__plan-feature">
-              crazy good 24/7 support
-            </p>
-            <p className="card--pricing__plan-feature">
-              top-notch cybersecurity
-            </p>
-            <p className="card--pricing__plan-feature">
-              robots with sick moves
-            </p>
+            <PricingCardContent
+              name="Enterprise"
+              emoji={bigFishEmoji}
+              alt="enterprise plan emoji"
+              features={[
+                'all the basic and pro features',
+                '+',
+                'more bling bling',
+                'crazy good 24/7 support',
+                'top-notch cybersecurity',
+                'robots with sick moves',
+              ]}
+            />
           </Card>
         </div>
       </Centrifier>
