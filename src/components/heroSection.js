e74e533557790fbb16f61def4f0a42fd7f128cc2
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-24 15:04:21 +0100

    refactored hero transitions to remove nesting in render function

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -7,6 +7,28 @@ import Centrifier from './helperComponents/centrifier';
 
 import './heroSection.sass';
 
+const TitleTransition = ({ children, show }) => (
+  <Transition
+    items={show}
+    from={{ opacity: 0 }}
+    enter={{ opacity: 1 }}
+    config={{ duration: 1500 }}
+  >
+    {show => show && (props => <div style={props}>{children}</div>)}
+  </Transition>
+);
+
+const SubTitleTransition = ({ children, show }) => (
+  <Transition
+    items={show}
+    from={{ opacity: 0, transform: 'translateY(50px)' }}
+    enter={{ opacity: 1, transform: 'translateY(0px)' }}
+    config={{ friction: 50, delay: 1100 }}
+  >
+    {show => show && (props => <div style={props}>{children}</div>)}
+  </Transition>
+);
+
 class HeroSection extends Component {
   state = {
     show: false,
@@ -20,58 +42,22 @@ class HeroSection extends Component {
         <Container>
           <Centrifier>
             <div className="hero-section__content">
-              <Transition
-                items={this.state.show}
-                from={{ opacity: 0 }}
-                enter={{ opacity: 1 }}
-                config={{ duration: 1500 }}
-              >
-                {show =>
-                  show &&
-                  (props => (
-                    <Fragment>
-                      <p
-                        style={props}
-                        className="hero-section__content__heading"
-                      >
-                        Stones
-                      </p>
-                      <p
-                        style={props}
-                        className="hero-section__content__heading"
-                      >
-                        Redefined
-                      </p>
-                    </Fragment>
-                  ))
-                }
-              </Transition>
-
+              <TitleTransition show={this.state.show}>
+                <p className="hero-section__content__heading">Stones</p>
+                <p className="hero-section__content__heading">Redefined</p>
+              </TitleTransition>
               <Centrifier>
-                <Transition
-                  items={this.state.show}
-                  from={{ opacity: 0, transform: 'translateY(50px)' }}
-                  enter={{ opacity: 1, transform: 'translateY(0px)' }}
-                  config={{ friction: 50, delay: 1100 }}
-                >
-                  {show =>
-                    show &&
-                    (props => (
-                      <div
-                        style={props}
-                        className="hero-section__content__text"
-                      >
-                        <span aria-label="pointing finger" role="img">
-                          <Img
-                            alt="pointing finger"
-                            fluid={this.props.fingerEmoji.childImageSharp.fluid}
-                          />
-                        </span>
-                        Sign up for trial
-                      </div>
-                    ))
-                  }
-                </Transition>
+                <SubTitleTransition show={this.state.show}>
+                  <div className="hero-section__content__text">
+                    <span aria-label="pointing finger" role="img">
+                      <Img
+                        alt="pointing finger"
+                        fluid={this.props.fingerEmoji.childImageSharp.fluid}
+                      />
+                    </span>
+                    Sign up for trial
+                  </div>
+                </SubTitleTransition>
               </Centrifier>
             </div>
           </Centrifier>
